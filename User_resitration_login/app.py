ca4109401b6182d257efbbc5749a924cd97b3c00
Karishma <kshinde.sawant@gmail.com>
2021-01-07 16:34:32 +0530

    register view Added

--- User_resitration_login/app.py
+++ User_resitration_login/app.py
@@ -14,3 +14,21 @@ mysql = MySQL(app)
 @app.route('/')
 def home():
     return render_template("home.html")
+
+
+@app.route('/register', methods=["GET", "POST"]) 
+def register():
+    if request.method == 'GET':
+        return render_template("register.html")
+    else:
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password'].encode('utf-8')
+        hash_password = bcrypt.hashpw(password, bcrypt.gensalt())
+ 
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users (name, email, password) VALUES (%s,%s,%s)",(name,email,hash_password,))
+        mysql.connection.commit()
+        session['name'] = request.form['name']
+        session['email'] = request.form['email']
+        return redirect(url_for('home'))
